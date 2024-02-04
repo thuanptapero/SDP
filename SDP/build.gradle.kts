3cb7938e514cacc7aa35ab02aefe928d3860de80
noRoom113 <tienthuan05082002@gmail.com>
2024-02-04 23:21:52 +0700

    init

--- SDP/build.gradle.kts
+++ SDP/build.gradle.kts
@@ -48,9 +48,8 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
-    implemention("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
